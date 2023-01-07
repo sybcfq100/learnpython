446485617d9acfc6285a0cb82fd2ff4f8cdcb54d
sybcfq100 <46489758+sybcfq100@users.noreply.github.com>
2023-01-07 21:11:16 +0800

    first commit

---
+++ Day7.py
@@ -0,0 +1,48 @@
+# import os
+# import time
+
+# def main():
+#     content = '北京欢迎您，为您开天辟地......'
+#     while True:
+#         os.system('cls')
+#         print(content)
+#         time.sleep(0.2)
+#         content = content[1:]+content[0]
+
+        
+        
+# if __name__ == '__main__':
+#     main()
+
+a, b = 5, 10
+print(f'{a} * {b} = {a*b}')
+
+# list1 = [1,3,5,7,100]
+# for index in range(len(list1)):
+#     print(list1[index])
+
+# for elem in list1:
+#     print(elem)
+    
+# for index, elem in enumerate(list1):
+#     print(index, '\t', elem)
+
+# list1.append(200)
+# print(list1)
+# list1.insert(2, 400)
+# print(list1)
+# print(len(list1))
+# if 1234 in list1:
+#     list1.remove(3)
+# print(list1)
+# list1.pop(0)
+# print(list1)
+
+# import sys
+# f = (x**2 for x in range(1, 1000))
+# print(sys.getsizeof(f))
+# print(f)
+# for val in f:
+#     print(val)
+
+
---
+++ usefulVimNet.md
@@ -0,0 +1,19 @@
+${toc}
+# charpt 1
+##  有用的网址 关于vim的
+
+1. [vim 操作指令_狂吃不胖温同学的博客-CSDN博客](https://blog.csdn.net/weixin_46574961/article/details/108951063)
+2. [ubuntu下vim的配置文件.vimrc的创建（转）_teffi的专栏-CSDN博客_ubuntu vim配置文件](https://blog.csdn.net/teffi/article/details/53642504)
+3. [ubuntu错误：Failed to connect to raw.githubusercontent.com port 443: Connection refused_morning_berry的博客-CSDN博客](https://blog.csdn.net/morning_berry/article/details/105517538)
+4. https://github.com/theniceboy/nvim  ：/TheCW up主的设置
+5. https://blog.csdn.net/qq_42138566/article/details/82800451  ：/ vimplus
+6. https://github.com/theniceboy
+
+## what did you do?
+- [ ] lunch
+- [ ] breakfast
+- [ ] do homework
+## Todo what
+> what does make you so powerful?
+- how to do homework
+
