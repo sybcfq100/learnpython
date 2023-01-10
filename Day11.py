eeb1a103c458b9aa705b2c7683c604f1e4b277bb
sybcfq100 <sybcfq100@gmail.com>
2023-01-10 20:29:05 +0800

    shake

--- Day11.py
+++ Day11.py
@@ -37,39 +37,44 @@
 #     main()
 
 
-from math import sqrt
+# from math import sqrt
 
 
-def is_prime(n):
-    """判断素数的函数"""
-    assert n > 0
-    return next(
-        (False for factor in range(2, int(sqrt(n)) + 1) if n % factor == 0),
-        n != 1,
-    )
+# def is_prime(n):
+#     """判断素数的函数"""
+#     assert n > 0
+#     return next(
+#         (False for factor in range(2, int(sqrt(n)) + 1) if n % factor == 0),
+#         n != 1,
+#     )
 
 
-def main():
-    filenames = ('a.txt', 'b.txt', 'c.txt')
-    fs_list = []
-    try:
-        fs_list.extend(open(filename, 'w', encoding='utf-8') for filename in filenames)
-        for number in range(1, 10000):
-            if is_prime(number):
-                if number < 100:
-                    fs_list[0].write(str(number) + '\n')
-                elif number < 1000:
-                    fs_list[1].write(str(number) + '\n')
-                else:
-                    fs_list[2].write(str(number) + '\n')
-    except IOError as ex:
-        print(ex)
-        print('写文件时发生错误!')
-    finally:
-        for fs in fs_list:
-            fs.close()
-    print('操作完成!')
+# def main():
+#     filenames = ('a.txt', 'b.txt', 'c.txt')
+#     fs_list = []
+#     try:
+#         fs_list.extend(open(filename, 'w', encoding='utf-8') for filename in filenames)
+#         for number in range(1, 10000):
+#             if is_prime(number):
+#                 if number < 100:
+#                     fs_list[0].write(str(number) + '\n')
+#                 elif number < 1000:
+#                     fs_list[1].write(str(number) + '\n')
+#                 else:
+#                     fs_list[2].write(str(number) + '\n')
+#     except IOError as ex:
+#         print(ex)
+#         print('写文件时发生错误!')
+#     finally:
+#         for fs in fs_list:
+#             fs.close()
+#     print('操作完成!')
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+
+shakes = open('shakespeare.txt')
+text = shakes.read().split()
+len(text)
