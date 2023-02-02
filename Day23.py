0e17940339675d7353bd194fbe9cfe5d8e5d7d5e
sybcfq100 <sybcfq100@gmail.com>
2023-02-02 15:03:42 +0800

    不太懂

--- Day23.py
+++ Day23.py
@@ -1,9 +1,22 @@
-def sum_squares(a, b):
-    return square(a) + square(b)
+# def sum_squares(a, b):
+#     return square(a) + square(b)
 
-def f(x):
-    return x * x
+# def f(x):
+#     return x * x
 
-square = f
-y = sum_squares(2, 3)
-print(y)
+# square = f
+# y = sum_squares(2, 3)
+# print(y)
+
+def view(re):
+    ice = 4
+    def k(means):
+        return re(means)
+    return k
+
+def review(pr):
+    return re(pr * 2)
+
+ice = 3
+re = view(lambda pr: pr * ice)
+print(review(1))
