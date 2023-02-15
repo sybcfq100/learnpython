1f9d3dbe4b23137398f831bcc42d0f5458418b14
sybcfq100 <sybcfq100@gmail.com>
2023-02-15 21:37:34 +0800

    min 的特殊用法

--- Day29.py
+++ Day29.py
@@ -13,5 +13,16 @@
 #  w1 = 'appear'
 #  print(add_chars(w1, w2))
 
-g = lambda n: (lambda f: f(n, f))(lambda n, fact: 1 if n ==1 else n * fact(n-1, fact))
-print(g(4))
+# g = lambda n: (lambda f: f(n, f))(lambda n, fact: 1 if n ==1 else n * fact(n-1, fact))
+# print(g(4))
+a1 = min([3,2,5,6])
+print(a1)
+
+a2 = min([3,2,5,6], key=lambda x: abs(x*x-24))
+print(a2)
+f = lambda x: abs(x*x-24)
+t = [f(x) for x in [3,2,5,6]]
+print(t)
+d = [[f(x),x] for x in [3,2,5,6]]
+md = min(d)
+print(md)
