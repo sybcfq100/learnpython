f9d9605b2707232c38d182cda1e6c9a703993455
sybcfq100 <sybcfq100@gmail.com>
2023-02-19 18:19:37 +0800

    generator

---
+++ IterDay32.py
@@ -0,0 +1,38 @@
+def double(x):
+    print('**', x, '=>', 2*x, '**')
+    return 2*x
+
+
+m = map(double, range(3, 7))
+f = lambda y: y >= 10
+t = filter(f, m)
+print(next(t))
+
+print(list(filter(f, map(double, range(3, 7)))))
+
+def evens(start, end):
+    even = start + start % 2
+    while even < end:
+        yield even
+        even += 2
+
+print(list(evens(1, 10)))
+
+def countdown(k):
+    if k > 0:
+        yield k
+        yield from countdown(k-1)
+    else:
+        yield 'Blast off'
+
+def prefixes(s):
+    if s:
+        yield from prefixes(s[:-1])
+        yield s
+
+def substrings(s):
+    if s:
+        yield from prefixes(s)
+        yield from substrings(s[1:])
+
+print(list(substrings('tops')))
