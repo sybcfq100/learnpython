705d67fc0fd6af3d23a3bd5d302bd84b2e37c933
sybcfq100 <sybcfq100@gmail.com>
2023-03-23 10:32:26 +0800

    例子递归

---
+++ binary_sum.py
@@ -0,0 +1,14 @@
+def binary_sum(s, start, stop):
+    if start >= stop:
+        return 0
+    elif start == stop - 1:
+        return s[start]
+    else:
+        mid = (start + stop) // 2
+        return binary_sum(s, start, mid) + binary_sum(s, mid, stop)
+    
+s1 = list(range(10))
+s2 = list(range(5))
+s = s1 + s2
+
+print(binary_sum(s, 0, len(s)))
