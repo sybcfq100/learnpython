834d9300b76ffcce5ebef2b36d0a53a23dc4a32f
sybcfq100gmail <sybcfq100@gmail.com>
2023-04-14 12:07:37 +0800

    找零钱的方式

--- hw03/hw03/hw03.py
+++ hw03/hw03/hw03.py
@@ -92,77 +92,77 @@ def pingpong(n):
 #         return -step
 #     return step
 
-    i = 1
-    x = 1
-    is_up = True
-    while i < n:
-        is_up = next_dir(is_up, i)
-        if is_up:
-            x += 1
-        else:
-            x -= 1
-        i += 1
-    return x
+#     i = 1
+#     x = 1
+#     is_up = True
+#     while i < n:
+#         is_up = next_dir(is_up, i)
+#         if is_up:
+#             x += 1
+#         else:
+#             x -= 1
+#         i += 1
+#     return x
 
-def next_dir(is_up, i):
-    if i % 8 == 0 or num_eights(i) > 0:
-        return not is_up
-    return is_up
+# def next_dir(is_up, i):
+#     if i % 8 == 0 or num_eights(i) > 0:
+#         return not is_up
+#     return is_up
 
 
-print(pingpong(100))
+# print(pingpong(100))
 
-#  def next_larger_coin(coin):
-#      """Returns the next larger coin in order.
-#      >>> next_larger_coin(1)
-#      5
-#      >>> next_larger_coin(5)
-#      10
-#      >>> next_larger_coin(10)
-#      25
-#      >>> next_larger_coin(2) # Other values return None
-#      """
-#      if coin == 1:
-#          return 5
-#      elif coin == 5:
-#          return 10
-#      elif coin == 10:
-#          return 25
-#
-#
-#  def next_smaller_coin(coin):
-#      """Returns the next smaller coin in order.
-#      >>> next_smaller_coin(25)
-#      10
-#      >>> next_smaller_coin(10)
-#      5
-#      >>> next_smaller_coin(5)
-#      1
-#      >>> next_smaller_coin(2) # Other values return None
-#      """
-#      if coin == 25:
-#          return 10
-#      elif coin == 10:
-#          return 5
-#      elif coin == 5:
-#          return 1
-#
-#
-#  def count_coins(change):
-#      """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
-#      >>> count_coins(15)
-#      6
-#      >>> count_coins(10)
-#      4
-#      >>> count_coins(20)
-#      9
-#      >>> count_coins(100) # How many ways to make change for a dollar?
-#      242
-#      >>> count_coins(200)
-#      1463
-#      >>> from construct_check import check
-#      >>> # ban iteration
-#      >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
-#      True
-#      """
-#  "*** YOUR CODE HERE ***"
+ def next_larger_coin(coin):
+     """Returns the next larger coin in order.
+     >>> next_larger_coin(1)
+     5
+     >>> next_larger_coin(5)
+     10
+     >>> next_larger_coin(10)
+     25
+     >>> next_larger_coin(2) # Other values return None
+     """
+     if coin == 1:
+         return 5
+     elif coin == 5:
+         return 10
+     elif coin == 10:
+         return 25
+
+
+ def next_smaller_coin(coin):
+     """Returns the next smaller coin in order.
+     >>> next_smaller_coin(25)
+     10
+     >>> next_smaller_coin(10)
+     5
+     >>> next_smaller_coin(5)
+     1
+     >>> next_smaller_coin(2) # Other values return None
+     """
+     if coin == 25:
+         return 10
+     elif coin == 10:
+         return 5
+     elif coin == 5:
+         return 1
+
+
+ def count_coins(change):
+     """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+     >>> count_coins(15)
+     6
+     >>> count_coins(10)
+     4
+     >>> count_coins(20)
+     9
+     >>> count_coins(100) # How many ways to make change for a dollar?
+     242
+     >>> count_coins(200)
+     1463
+     >>> from construct_check import check
+     >>> # ban iteration
+     >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+     True
+     """
+ "*** YOUR CODE HERE ***"
