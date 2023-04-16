b191972a0be55df909254737bea510788d410c3d
sybcfq100 <sybcfq100@gmail.com>
2023-04-16 12:44:02 +0800

    研究change函数

--- hw03/hw03/hw03.py
+++ hw03/hw03/hw03.py
@@ -36,53 +36,53 @@
 #  print(num_eights(123456))
 
 
-def pingpong(n):
-    """Return the nth element of the ping-pong sequence.
-
-    >>> pingpong(8)
-    8
-    >>> pingpong(10)
-    6
-    >>> pingpong(15)
-    1
-    >>> pingpong(21)
-    -1
-    >>> pingpong(22)
-    -2
-    >>> pingpong(30)
-    -2
-    >>> pingpong(68)
-    0
-    >>> pingpong(69)
-    -1
-    >>> pingpong(80)
-    >>> pingpong(81)
-    1
-    >>> pingpong(82)
-    0
-    >>> pingpong(100)
-    -6
-    >>> from construct_check import check
-    >>> # ban assignment statements
-    >>> check(HW_SOURCE_FILE, 'pingpong',
-    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr'])
-    True
-    """
-
-    def helper(result, i, step=1):
-        if i == n:
-            return result
-        # elif i % 8 == 0 or num_eights(i) > 0:
-        elif i % 8 == 0 or "8" in str(i):
-            return helper(result - step, i + 1, -step)
-        else:
-            return helper(result + step, i + 1, step)
-
-    return helper(1, 1)
-
-
-print(pingpong(10))
-
+#  def pingpong(n):
+#      """Return the nth element of the ping-pong sequence.
+#
+#      >>> pingpong(8)
+#      8
+#      >>> pingpong(10)
+#      6
+#      >>> pingpong(15)
+#      1
+#      >>> pingpong(21)
+#      -1
+#      >>> pingpong(22)
+#      -2
+#      >>> pingpong(30)
+#      -2
+#      >>> pingpong(68)
+#      0
+#      >>> pingpong(69)
+#      -1
+#      >>> pingpong(80)
+#      >>> pingpong(81)
+#      1
+#      >>> pingpong(82)
+#      0
+#      >>> pingpong(100)
+#      -6
+#      >>> from construct_check import check
+#      >>> # ban assignment statements
+#      >>> check(HW_SOURCE_FILE, 'pingpong',
+#      ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr'])
+#      True
+#      """
+#
+#      def helper(result, i, step=1):
+#          if i == n:
+#              return result
+#          # elif i % 8 == 0 or num_eights(i) > 0:
+#          elif i % 8 == 0 or "8" in str(i):
+#              return helper(result - step, i + 1, -step)
+#          else:
+#              return helper(result + step, i + 1, step)
+#
+#      return helper(1, 1)
+#
+#
+#  print(pingpong(10))
+#
 
 # Alternate solution 1
 # def pingpong_next(x, i, step):
@@ -117,57 +117,69 @@ print(pingpong(10))
 
 # print(pingpong(100))
 
-#  def next_larger_coin(coin):
-#      """Returns the next larger coin in order.
-#      >>> next_larger_coin(1)
-#      5
-#      >>> next_larger_coin(5)
-#      10
-#      >>> next_larger_coin(10)
-#      25
-#      >>> next_larger_coin(2) # Other values return None
-#      """
-#      if coin == 1:
-#          return 5
-#      elif coin == 5:
-#          return 10
-#      elif coin == 10:
-#          return 25
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
 
 
-#  def next_smaller_coin(coin):
-#      """Returns the next smaller coin in order.
-#      >>> next_smaller_coin(25)
-#      10
-#      >>> next_smaller_coin(10)
-#      5
-#      >>> next_smaller_coin(5)
-#      1
-#      >>> next_smaller_coin(2) # Other values return None
-#      """
-#      if coin == 25:
-#          return 10
-#      elif coin == 10:
-#          return 5
-#      elif coin == 5:
-#          return 1
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
 
 
-#  def count_coins(change):
-#      """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
-#      >>> count_coins(15)
-#      6
-#      >>> count_coins(10)
-#      4
-#      >>> count_coins(20)
-#      9
-#      >>> count_coins(100) # How many ways to make change for a dollar?
-#      242
-#      >>> count_coins(200)
-#      1463
-#      >>> from construct_check import check
-#      >>> # ban iteration
-#      >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
-#      True
-#      """
-#  "*** YOUR CODE HERE ***"
+def count_coins(change):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    def constrained_count(change, smallest_coin):
+        if change == 0:
+            return 1
+        if change < 0:
+            return 0
+        if smallest_coin == None:
+            return 0
+        without_coin = constrained_count(change, next_larger_coin(smallest_coin))
+        with_coin = constrained_count(change-smallest_coin, smallest_coin)
+        return without_coin + with_coin
+    return constrained_count(change, 1) 
+
+print(count_coins(15))
