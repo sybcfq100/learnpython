df222e5d4f743fc80319706e5bf8ea71a82bd2c5
sybcfq100gmail <sybcfq100@gmail.com>
2023-04-23 20:01:59 +0800

    make_repeater

--- lab03/lab03.py
+++ lab03/lab03.py
@@ -8,71 +8,70 @@ triple = lambda x: 3 * x
 
 increment = lambda x: x + 1
 
-
 #  def ordered_digits(x):
-    #  """Return True if the (base 10) digits of X>0 are in non-decreasing
-    #  order, and False otherwise.
-    #
-    #  >>> ordered_digits(5)
-    #  True
-    #  >>> ordered_digits(11)
-    #  True
-    #  >>> ordered_digits(127)
-    #  True
-    #  >>> ordered_digits(1357)
-    #  True
-    #  >>> ordered_digits(21)
-    #  False
-    #  >>> result = ordered_digits(1375) # Return, don't print
-    #  >>> result
-    #  False
-    #
-    #  """
-    #  "*** YOUR CODE HERE ***"
-    #  for _ in range(len(str(x))):
-    #      rest, last = x // 10, x % 10
-    #      if rest % 10 > last:
-    #          return False
-    #      return True
-    # alternative solution
+#  """Return True if the (base 10) digits of X>0 are in non-decreasing
+#  order, and False otherwise.
+#
+#  >>> ordered_digits(5)
+#  True
+#  >>> ordered_digits(11)
+#  True
+#  >>> ordered_digits(127)
+#  True
+#  >>> ordered_digits(1357)
+#  True
+#  >>> ordered_digits(21)
+#  False
+#  >>> result = ordered_digits(1375) # Return, don't print
+#  >>> result
+#  False
+#
+#  """
+#  "*** YOUR CODE HERE ***"
+#  for _ in range(len(str(x))):
+#      rest, last = x // 10, x % 10
+#      if rest % 10 > last:
+#          return False
+#      return True
+# alternative solution
 #      last = x % 10
-    #  x = x // 10
-    #  while x > 0 and last >= x % 10:
-    #      last = x % 10
-    #      x = x // 10
-    #  return x == 0
+#  x = x // 10
+#  while x > 0 and last >= x % 10:
+#      last = x % 10
+#      x = x // 10
+#  return x == 0
 #  print(ordered_digits(13579))
 
-def get_k_run_starter(n, k):
-    """Returns the 0th digit of the kth increasing run within n.
-    >>> get_k_run_starter(123444345, 0) # example from description
-    3
-    >>> get_k_run_starter(123444345, 1)
-    4
-    >>> get_k_run_starter(123444345, 2)
-    4
-    >>> get_k_run_starter(123444345, 3)
-    1
-    >>> get_k_run_starter(123412341234, 1)
-    1
-    >>> get_k_run_starter(1234234534564567, 0)
-    4
-    >>> get_k_run_starter(1234234534564567, 1)
-    3
-    >>> get_k_run_starter(1234234534564567, 2)
-    2
-    """
-    i = 0
-    final = None
-    while i <= k:
-        while n > 10 and (n % 10) > (n // 10) % 10:
-            n = n // 10
-        final = n % 10
-        i = i + 1
-        n = n // 10
-    return final
+# def get_k_run_starter(n, k):
+#     """Returns the 0th digit of the kth increasing run within n.
+#     >>> get_k_run_starter(123444345, 0) # example from description
+#     3
+#     >>> get_k_run_starter(123444345, 1)
+#     4
+#     >>> get_k_run_starter(123444345, 2)
+#     4
+#     >>> get_k_run_starter(123444345, 3)
+#     1
+#     >>> get_k_run_starter(123412341234, 1)
+#     1
+#     >>> get_k_run_starter(1234234534564567, 0)
+#     4
+#     >>> get_k_run_starter(1234234534564567, 1)
+#     3
+#     >>> get_k_run_starter(1234234534564567, 2)
+#     2
+#     """
+#     i = 0
+#     final = None
+#     while i <= k:
+#         while n > 10 and (n % 10) > (n // 10) % 10:
+#             n = n // 10
+#         final = n % 10
+#         i = i + 1
+#         n = n // 10
+#     return final
 
-print(get_k_run_starter(123444345, 1))
+# print(get_k_run_starter(123444345, 1))
 
 
 def make_repeater(func, n):
@@ -91,67 +90,85 @@ def make_repeater(func, n):
     5
     """
     "*** YOUR CODE HERE ***"
+    def inner(x):
+        if n == 0:
+            return x
+        while n > 0:
+            x = func(x)
+            n = n -1
+        return x
+
+    return inner
+
+print(make_repeater(triple, 5)(1))
 
 
+# def composer(func1, func2):
+#     """Return a function f, such that f(x) = func1(func2(x))."""
 
-def composer(func1, func2):
-    """Return a function f, such that f(x) = func1(func2(x))."""
-    def f(x):
-        return func1(func2(x))
-    return f
+#     def f(x):
+#         return func1(func2(x))
 
-
-def apply_twice(func):
-    """ Return a function that applies func twice.
-
-    func -- a function that takes one argument
-
-    >>> apply_twice(square)(2)
-    16
-    """
-    "*** YOUR CODE HERE ***"
+#     return f
 
 
-def div_by_primes_under(n):
-    """
-    >>> div_by_primes_under(10)(11)
-    False
-    >>> div_by_primes_under(10)(121)
-    False
-    >>> div_by_primes_under(10)(12)
-    True
-    >>> div_by_primes_under(5)(1)
-    False
-    """
-    checker = lambda x: False
-    i = ____________________________
-    while ____________________________:
-        if not checker(i):
-            checker = ____________________________
-        i = ____________________________
-    return ____________________________
+# def apply_twice(func):
+#     """ Return a function that applies func twice.
+
+#     func -- a function that takes one argument
+
+#     >>> apply_twice(square)(2)
+#     16
+#     """
+#     "*** YOUR CODE HERE ***"
 
 
-def div_by_primes_under_no_lambda(n):
-    """
-    >>> div_by_primes_under_no_lambda(10)(11)
-    False
-    >>> div_by_primes_under_no_lambda(10)(121)
-    False
-    >>> div_by_primes_under_no_lambda(10)(12)
-    True
-    >>> div_by_primes_under_no_lambda(5)(1)
-    False
-    """
-    def checker(x):
-        return False
-    i = ____________________________
-    while ____________________________:
-        if not checker(i):
-            def outer(____________________________):
-                def inner(____________________________):
-                    return ____________________________
-                return ____________________________
-            checker = ____________________________
-        i = ____________________________
-    return ____________________________
+# def div_by_primes_under(n):
+#     """
+#     >>> div_by_primes_under(10)(11)
+#     False
+#     >>> div_by_primes_under(10)(121)
+#     False
+#     >>> div_by_primes_under(10)(12)
+#     True
+#     >>> div_by_primes_under(5)(1)
+#     False
+#     """
+#     checker = lambda x: False
+#     i = ____________________________
+#     while ____________________________:
+#         if not checker(i):
+#             checker = ____________________________
+#         i = ____________________________
+#     return ____________________________
+
+
+# def div_by_primes_under_no_lambda(n):
+#     """
+#     >>> div_by_primes_under_no_lambda(10)(11)
+#     False
+#     >>> div_by_primes_under_no_lambda(10)(121)
+#     False
+#     >>> div_by_primes_under_no_lambda(10)(12)
+#     True
+#     >>> div_by_primes_under_no_lambda(5)(1)
+#     False
+#     """
+
+#     def checker(x):
+#         return False
+
+#     i = ____________________________
+#     while ____________________________:
+#         if not checker(i):
+
+#             def outer(____________________________):
+
+#                 def inner(____________________________):
+#                     return ____________________________
+
+#                 return ____________________________
+
+#             checker = ____________________________
+#         i = ____________________________
+#     return ____________________________
