fc936a416ed4b55eb284bee87db52c5f23506c76
sybcfq100 <sybcfq100@gmail.com>
2023-06-13 20:33:09 +0800

    不懂yield在for中的用法

--- class/CloseDay37.py
+++ class/CloseDay37.py
@@ -1,13 +1,14 @@
-#  def everything(n):
-#      if n == 0:
-#          return 0
-#      else:
-#          for rest in everything(n//10):
-#              yield rest
-#              yield 10 * rest + n%10
-#  a = everything(98357)
-#  print(list(a))
-#
+def everything(n):
+    if n == 0:
+        return 0
+    for rest in everything(n // 10):
+        yield rest
+        yield 10 * rest + n % 10
+
+
+print(list(everything(98357)))
+
+
 #  def increasing(n, smallest=10):
 #      if n == 0:
 #          return 0
@@ -18,14 +19,13 @@
 #      else:
 #          return increasing(n//10, smallest)
 
+# def close(n, smallest = 10, d = 10):
+#     if n == 0:
+#         return 0
+#     no = close(n//10, smallest, d)
+#     if smallest > n % 10:
+#         yes = close(n//10, min(smallest, d), n %10 )*10 + n%10
+#         return max(yes, no)
+#     return no
 
-def close(n, smallest = 10, d = 10):
-    if n == 0:
-        return 0
-    no = close(n//10, smallest, d)
-    if smallest > n % 10:
-        yes = close(n//10, min(smallest, d), n %10 )*10 + n%10 
-        return max(yes, no)
-    return no
-
-print(close(45671))
+# print(close(45671))
