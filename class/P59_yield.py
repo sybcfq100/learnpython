bfd3f0483f3d306a4d79ef04bcdf6a70e5499da0
sybcfq100 <46489758+sybcfq100@users.noreply.github.com>
2023-06-14 14:30:45 +0800

    虽然不懂，但是可以运行

--- class/P59_yield.py
+++ class/P59_yield.py
@@ -1,13 +1,19 @@
 def everything(n):
+    """ Yield every number "within" n."""
     if n == 0:
-        return 0
-    for rest in everything(n // 10):
-        yield rest
-        yield 10 * rest + n % 10
+        # return n
+        yield 0
+        # yield 0 # or yield n --it's same 
+    # yield from everything(n // 10)
+    else:
+        for rest in everything(n // 10):
+            yield rest
+            yield 10 * rest + n % 10
 
 
-print(list(everything(98357)))
-
+a = everything(1234)
+print(list(a))
+# print(list(everything(98357)))
 
 #  def increasing(n, smallest=10):
 #      if n == 0:
