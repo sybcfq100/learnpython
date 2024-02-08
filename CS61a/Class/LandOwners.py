fbb31d8f77542d6496c7b4cead43f76cf4092704
sybcfq100 <46489758+sybcfq100@users.noreply.github.com>
2024-02-08 19:32:38 +0800

    ok

--- CS61a/Class/LandOwners.py
+++ CS61a/Class/LandOwners.py
@@ -1,23 +1,27 @@
 class Worker:
-    greeting = 'Sir'
+    greeting = "Sir"
+
     def __init__(self):
         self.elf = Worker
 
     def work(self):
-        return self.greeting + ', I work'
+        return f"{self.greeting}, I work"
 
     def __repr__(self):
         return Bourgeoisie.greeting
 
+
 class Bourgeoisie(Worker):
-    greeting = 'Peon'
+    greeting = "Peon"
+
     def work(self):
         print(Worker.work(self))
-        return 'I gather weather'
+        return "I gather weather"
+
 
 jack = Worker()
 john = Bourgeoisie()
-jack.greeting = 'Maam'
+jack.greeting = "Maam"
 print(Worker().work())
 print(jack)
 print(jack.work())
