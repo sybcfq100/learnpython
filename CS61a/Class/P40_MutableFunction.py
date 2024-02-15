bc8ca5f1a5678e07378980cc1c2f6333ae8c4d1d
sybcfq100 <46489758+sybcfq100@users.noreply.github.com>
2024-02-15 19:02:08 +0800

    ok

--- CS61a/Class/P40_MutableFunction.py
+++ CS61a/Class/P40_MutableFunction.py
@@ -1,13 +1,13 @@
 #  def make_withdraw(balance):
-    #  '''return a withdraw function with a starting balance'''
-    #  def withdraw(amount):
-    #      nonlocal balance # declare the name 'balance' nonlocal at the top of the body of the function in which it is re-assigned
-    #      if amount > balance:
-    #          return 'Insufficient funds'
-    #      balance = balance - amount # re-bind balance in the first non-local frame in which is was bound previously
-    #      return balance
-    #  return withdraw
-    #
+#  '''return a withdraw function with a starting balance'''
+#  def withdraw(amount):
+#      nonlocal balance # declare the name 'balance' nonlocal at the top of the body of the function in which it is re-assigned
+#      if amount > balance:
+#          return 'Insufficient funds'
+#      balance = balance - amount # re-bind balance in the first non-local frame in which is was bound previously
+#      return balance
+#  return withdraw
+#
 #  withdraw = (make_withdraw(100))
 #  print(withdraw(10))
 #  print(withdraw(35))
@@ -16,8 +16,10 @@
 #  what is the meaning or function of nonlocal
 #  """
 
+
 def f(x):
     x = 4
+
     def g(y):
         def h(z):
             nonlocal x
@@ -27,6 +29,7 @@ def f(x):
         return h
 
     return g
+
 
 a = f(1)
 b = a(2)
