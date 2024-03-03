c25f5e533a33a84120cd4a7f9fc07a3c93c57a9e
sybcfq100 <46489758+sybcfq100@users.noreply.github.com>
2024-03-03 18:16:10 +0800

    ok

--- Other/CaeserCipher.py
+++ Other/CaeserCipher.py
@@ -3,10 +3,10 @@ class CaesarCiher:
         encoder = [None] * 26
         decoder = [None] * 26
         for k in range(26):
-            encoder[k] = chr((k + shift) % 26 + ord('A'))
-            decoder[k] = chr((k - shift) % 26 + ord('A'))
-        self._forward = ''.join(encoder)
-        self._backward = ''.join(decoder)
+            encoder[k] = chr((k + shift) % 26 + ord("A"))
+            decoder[k] = chr((k - shift) % 26 + ord("A"))
+        self._forward = "".join(encoder)
+        self._backward = "".join(decoder)
 
     def encrypt(self, message):
         return self._transform(message, self._forward)
@@ -18,17 +18,15 @@ class CaesarCiher:
         msg = list(original)
         for k in range(len(msg)):
             if msg[k].isupper():
-                j = ord(msg[k]) - ord('A')
+                j = ord(msg[k]) - ord("A")
                 msg[k] = code[j]
-        return ''.join(msg)
+        return "".join(msg)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cipher = CaesarCiher(3)
     message = "THE EAGLE IS IN PLAY; MEET AT JOE'S."
     coded = cipher.encrypt(message)
-    print('Secret: ', coded)
+    print("Secret: ", coded)
     answer = cipher.decrypt(coded)
-    print('Message: ', answer)
-
-
-
+    print("Message: ", answer)
